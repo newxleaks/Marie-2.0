3774a0c2e0286159f3a28f9c463fe84056c566ec
newxleaks <63515312+newxleaks@users.noreply.github.com>
2020-04-14 02:06:23 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,7 +22,9 @@ hoi {}, my name is {}!
 
 im a group manager bot created by  [Stella](tg://user?id={}).
 
-For more commands click /help more
+For more commands click /help 
+
+Subscribe Our Channel [HotStuffs](t.me/hotstuffz)
 
 """
 
